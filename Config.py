feb02d1a2d6cd8f8abb1df1bf2c8b2605e220b97
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-07-04 17:48:59 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    API_ID = int(os.environ.get("APP_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)

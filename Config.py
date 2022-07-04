f1852249d5180ebf36b4672c0d1a1dd0a15f2dab
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-07-04 04:28:31 -0700

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config(object):
+    API_ID = int(os.environ.get("APP_ID", "6435225"))
+    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    SUPPORT = os.environ.get("SUPPORT", "JEPTHONSUPPORT")
+    CHANNEL = os.environ.get("CHANNEL", "JEPTHON")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/53d0addd5da72bf618c02.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/53d0addd5da72bf618c02.jpg")

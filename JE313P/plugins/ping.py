bc60c531cedf02203e68eb7515b6c8c184bcd69d
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-07-07 11:38:34 +0300

    لبيك يامهدي

--- JE313P/plugins/ping.py
+++ JE313P/plugins/ping.py
@@ -10,13 +10,13 @@ IMG = os.environ.get(
 ms = 4
 
 ALIVE = os.environ.get(
-    "ALIVE", "@JE313P"
+    "ALIVE", "@lMl10l"
 )
 
 CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
 
 
-@JE313P.on(events.NewMessage(pattern="^/ping"))
+@JE313P.on(events.NewMessage(pattern="^/بنك"))
 async def _(event):
     UMM = [[Button.url("السورس", "https://t.me/JEPTHON")]]
     await JE313P.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)

20a760696b545f40f8d4f7a5aa15c5a4f232d91d
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2023-08-07 10:53:30 -0700

    Update __main__.py

--- JE313P/__main__.py
+++ JE313P/__main__.py
@@ -5,6 +5,8 @@ import glob
 from pathlib import Path
 from JE313P.utils import load_plugins
 import logging
+from telethon.errors.rpcerrorlist import ChannelPrivateError
+from telethon.tl.functions.channels import JoinChannelRequest
 from JE313P import JE313P
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
@@ -18,6 +20,16 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
     
+HuRe = {"@jepthon", "@jepthonsupport"}
+async def saves():
+   for lMl10l in HuRe:
+        try:
+             await l313l(JoinChannelRequest(channel=lMl10l))
+        except OverflowError:
+            LOGS.error("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
+            continue
+        except ChannelPrivateError:
+            continue
 print("تم تنصيب السورس بنجاح")
 print("قناة السورس @Jepthon")
 
